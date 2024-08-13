7f2de3c3fb227f736389fbc982c8826e0fe3d483
laalmeidariberi <rlarissa191@gmail.com>
2024-08-13 14:16:14 -0300

    criando versao 2.0 do codigo

---
+++ codigo_Morse02.py
@@ -0,0 +1,16 @@
+
+import os
+
+def exibir_nome_do_programa():
+    print('''
+█▀▄ █▀▀ █▀▀ █ █▀▀ █▀█ ▄▀█ █▄░█ █▀▄ █▀█   █▀▀ █▀█ █▀▄ █ █▀▀ █▀█   █▀▄▀█ █▀█ █▀█ █▀ █▀▀
+█▄▀ ██▄ █▄▄ █ █▀░ █▀▄ █▀█ █░▀█ █▄▀ █▄█   █▄▄ █▄█ █▄▀ █ █▄█ █▄█   █░▀░█ █▄█ █▀▄ ▄█ ██▄''')
+
+
+def main():
+    ''' Função principal que inicia o programa '''
+    os.system('cls')
+    exibir_nome_do_programa()
+    
+if __name__ == '__main__':
+    main()
