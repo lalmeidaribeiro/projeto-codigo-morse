44312cc30e9a2d2a0101517eda69abfc6c315a06
laalmeidariberi <rlarissa191@gmail.com>
2024-08-13 16:07:58 -0300

    funcao decodifica_codigo_morse

--- codigo_Morse02.py
+++ codigo_Morse02.py
@@ -1,5 +1,6 @@
 
 import os
+#.-  .- ..- .-.. .-  -.. .-  -.-. .- .-. --- .-.. .. -. .-  ..  -- .- .-. .- ...- .. .-.. .... --- ... .-
 
 def exibir_nome_do_programa():
     print('''
@@ -12,26 +13,46 @@ def exibir_nome_do_programa():
 
 def exibir_opcoes_menu():
     print('Escololha uma das opções abaixo: \n')
-    print('1. Criar uma mensagem em código Morse')
+    print('1. Converte uma palavra ou frase em código Morse')
     print('2. Decifrar um código Morse')
     print('3. Sair do programa\n')
 
+def carrega_codigo_morse(): #Função para carregar o dicionário Morse
+    dicionario_morse = {".-": "A", "-...": "B", "-.-.": "C", "-..": "D", ".": "E", "..-.": "F","--.": "G", "....": "H", "..": "I", ".---": "J", "-.-": "K", ".-..": "L","--": "M", "-.": "N", "---": "O", ".--.": "P", "--.-": "Q", ".-.": "R","...": "S", "-": "T", "..-": "U", "...-": "V", ".--": "W", "-..-": "X","-.--": "Y", "--..": "Z", "-----": 0, ".----": 1, "..---": 2, "...--": 3,"....-": 4, ".....": 5, "-....": 6, "--...": 7, "---..": 8, "----.": 9}
+    return dicionario_morse
+
+def decodifica_codigo_morse(): #Função para decodificar o código morse
+    mensagem = input('Digite a mensagem em código Morse: ')
+    codigo_morse = carrega_codigo_morse()
+
+    #print(codigo_morse) #sertificando que careguei o dicionario para dentro da função 
+
+    palavras = mensagem.split('  ') #Separar palavras - Dois espaços
+    mensagem_decodificada = []
+
+    for palavra in palavras:
+        letras = palavra.split(' ')  # Separar letras - Um espaço 
+        palavra_decodificada = ''.join([codigo_morse.get(letra, '') for letra in letras])
+        mensagem_decodificada.append(palavra_decodificada)
+#Melhorar essa linha de código 
+    #print(mensagem_decodificada) #Retorna a lista 
+    return ' '.join(mensagem_decodificada)
+
+
 def escolha_opcao():
-    '''Exibi a solicitação de cada função que deve ser escolhida pelo usuário
-    Outputs:
-    -Executa a opção escolhida pelo usuário.
-    '''
     opcao_escolhida = int(input('Escolha uma opção: '))
 
     if opcao_escolhida == 1:
-        print('Você escolheu a opção 01')
+        print('Escolheu a opção 1')
+
     elif opcao_escolhida == 2:
-        print('Você escolheu a opção 02')
+        decodifica_codigo_morse()
+
     elif opcao_escolhida == 3:
-        print('você escolheu a opcao 03')
+        print('Finalizando o programa...')
+
     else:
         print('Sinto muito, essa não é uma opcao válida!')
-
 
 def main():
     ''' Função principal que inicia o programa '''
